c1781ab4f42dc9808d8920923529f0fcfaeacf00
Crisan Marius <crisan_mariusvlad@yahoo.com>
2014-06-22 16:16:18 +0300

    refactored search feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,5 @@
 class HomeController < ApplicationController
   def index
-      @videos = Posts::Video.all
-  end
-  
-  def search
-      @query = params[:query]
-      @videos = Posts::Video.where('LOWER(title) LIKE ?', "%#{params[:query].downcase}%")
+    @videos = Posts::Video.all
   end
 end
