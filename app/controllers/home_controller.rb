6a2dbd712686b135e1288b6c0782c9c56ea353eb
Crisan Marius <crisan_mariusvlad@yahoo.com>
2014-06-22 15:28:25 +0300

    created search feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @videos = Posts::Video.all
+      @videos = Posts::Video.all
+  end
+  
+  def search
+      @query = params[:query]
+      @videos = Posts::Video.where('LOWER(title) LIKE ?', "%#{params[:query].downcase}%")
   end
 end
