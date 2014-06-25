ded4665eb0bc1aff6a29073e0fa34ec353ef5a0d
Crisan Ciprian <crisanciprian_sile@yahoo.com>
2014-06-25 23:16:11 +0300

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :categories
+
+  def authenticate_admin_user!
+    redirect_to '/' and return if user_signed_in? && !current_user.admin?
+
+    authenticate_user!
+  end
+
+  def current_admin_user
+    return nil if user_signed_in? && !current_user.admin?
+    current_user
+  end
+
+  private
+
+  def categories
+    @categories ||= Category.all
+  end
 end
---
+++ config/initializers/rack_mini_profiler.rb
@@ -0,0 +1,1 @@
+Rack::MiniProfiler.config.skip_paths << "/system" if defined?(Rack::MiniProfiler)
