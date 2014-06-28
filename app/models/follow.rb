1cfba614e14e12a0b52d2ac86a31ffda2731f810
Crisan Ciprian <crisanciprian_sile@yahoo.com>
2014-06-28 18:32:18 +0300

    follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,6 +3,8 @@ class Follow < ActiveRecord::Base
   extend ActsAsFollower::FollowerLib
   extend ActsAsFollower::FollowScopes
 
+  attr_accessible :followable_id, :followable_type
+
   # NOTE: Follows belong to the "followable" interface, and also to followers
   belongs_to :followable, :polymorphic => true
   belongs_to :follower,   :polymorphic => true
