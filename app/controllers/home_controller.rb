5f7157239956a9e215f255d1d631cb05ca4e7077
Crisan Marius <crisan_mariusvlad@yahoo.com>
2014-07-02 00:04:14 +0300

    user feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @videos = Posts::Video.all
+    if user_signed_in?
+      @videos = Posts::Video.joins('INNER JOIN follows ON posts.user_id = follows.followable_id').
+                             where('follows.follower_id=?', current_user.id ).
+                             order('posts.created_at DESC')
+    else
+      @videos = Posts::Video.all
+    end 
   end
 end
