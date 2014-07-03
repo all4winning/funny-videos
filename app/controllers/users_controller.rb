31b31def48795404d5d579ef730fef8ba81dae7f
Crisan Marius <crisan_mariusvlad@yahoo.com>
2014-07-03 21:13:27 +0300

    user feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def feed
     @videos = Posts::Video.joins('LEFT OUTER JOIN follows ON posts.user_id = follows.followable_id').
                            joins('INNER JOIN categories_posts on posts.id=categories_posts.post_id').
-                           joins('INNER JOIN interests ON categories_posts.category_id = interests.category_id').
+                           joins('LEFT OUTER JOIN interests ON categories_posts.category_id = interests.category_id').
                            where('(follows.follower_id=?)OR(interests.selected=TRUE and interests.user_id=?)', current_user.id, current_user.id) 
   end
 
