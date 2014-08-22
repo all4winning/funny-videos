7972c390c997e2b72164769fd52448fa957b35f6
Crisan Marius <crisan_mariusvlad@yahoo.com>
2014-08-22 21:50:32 +0300

    end video message

--- app/assets/javascripts/youtube_player.js
+++ app/assets/javascripts/youtube_player.js
@@ -1,64 +1,56 @@
-// 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-      	var id = $("#player").data("id");
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: id,
-          events: {
-            //'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      // when video ends
-        function onPlayerStateChange(event) {        
-            if(event.data === 0) {          
-                $("iframe").hide();
-                $(".videomessage").show();
-                $(".video-facebook_connect").hide();
-                
-                
-                $.ajax({
-			        type: "POST",
-			        url: "/posts/videos/video_counter",
-			        cache: false,
-			        success: function(data) {
-			        	console.log("succ");
-			          }
-			    });
-			    
-			    $(".fclick").on("click", function(){
-			    	$.ajax({
-			        type: "POST",
-			        url: "/posts/videos/clicks",
-			        cache: false,
-			        success: function(data) {
-			        	console.log("succ");
-			          }
-			    });
-			   });
-            }
-        }
-  //    function stopVideo() {
-  //      player.stopVideo();
-  //    }
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+	var id = $("#player").data("id");
+	player = new YT.Player('player', {
+	  height: '390',
+	  width: '640',
+	  videoId: id,
+	  events: {
+		   'onStateChange': onPlayerStateChange
+	      },
+	  loop: 1,
+	  });
+}
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  // when video ends
+function onPlayerStateChange(event) {        
+    if(event.data === 0) {   
+    	var imageUrl = $("#player").data("image");
+		$(".video-container").css({'background-image': 'url(' + imageUrl + ')',
+					'background-repeat': 'no-repeat',
+					'background-position': 'center',
+					'background-size': 'cover'});
+		$("iframe").hide();
+		$(".videomessage").show();
+		$(".video-facebook_connect").hide();
+		
+		$(".replay").on("click", function(){
+			$("iframe").show();
+		    $(".videomessage").hide();
+		    $(".video-facebook_connect").show();
+		    event.target.playVideo();
+		});
+		$.ajax({
+		    type: "POST",
+		    url: "/posts/videos/video_counter",
+		    cache: false,
+		});
+		$(".fclick").on("click", function(event){
+			//event.preventDefault();
+			$.ajax({
+		    type: "POST",
+		    url: "/posts/videos/clicks",
+					        cache: false,
+					    });
+					   });
+		            }
+		        };
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @videos = Posts::Video.published.paginate(:page => params[:page], :per_page => Rails.configuration.videos_per_page)
+    @videos = Posts::Video.order(created_at: :desc).published.paginate(:page => params[:page], :per_page => Rails.configuration.videos_per_page)
     @popular_videos = Posts::Video.popular_videos.published.limit(6)
   end
 end
